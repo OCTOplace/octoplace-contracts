9b33a74336b0002031ad0dc070bd7edea2faf87a
Shivam Singh <shivamsingh071093@gmail.com>
2023-05-16 17:10:12 +0530

    sell contract added

--- contracts/example2981NFT.sol
+++ contracts/example2981NFT.sol
@@ -1,10 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol';
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 
 /// @dev This is a contract used to add ERC2981 support to ERC721 and 1155
 /// @dev This implementation has the same royalties for each and every tokens
-contract ERC2981ContractWideRoyalties is ERC2981Base {
-    
+contract NFTexample is ERC721 {
+    constructor () ERC721("NFT Example", "NEFT"){
+        for(uint8 i = 0; i < 10 ; i++){
+            _safeMint(i+1, msg.sender)
+        }
+    }
 }
